ea71b3a2a819d2bad5cf95157023f6babc208c22
Akki-art-byte <akash1996srivastav@gmail.com>
2024-07-07 11:33:13 +0530

    Add files via upload

---
+++ oops/Abstraction.java
@@ -0,0 +1,18 @@
+package oops;
+
+abstract class Abstraction {
+	
+	
+
+	public Abstraction() {
+		super();
+	}
+
+	public int multiply(int a, int b) {
+		System.out.println("multiply of a and b" + " = " + a * b);
+		return a * b;
+	}
+
+	public abstract void divide(int a,int b);
+
+}
